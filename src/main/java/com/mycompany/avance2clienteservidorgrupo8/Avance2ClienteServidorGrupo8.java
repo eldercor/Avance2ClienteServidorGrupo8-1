41f2fd271c9ad60549a5ac42ff4765b1fe963c2d
eldercor <ecorella70943@ufide.ac.cr>
2024-11-17 23:26:16 -0600

    Update Avance2ClienteServidorGrupo8.java

--- src/main/java/com/mycompany/avance2clienteservidorgrupo8/Avance2ClienteServidorGrupo8.java
+++ src/main/java/com/mycompany/avance2clienteservidorgrupo8/Avance2ClienteServidorGrupo8.java
@@ -1,8 +1,19 @@
 package com.mycompany.avance2clienteservidorgrupo8;
 
-public class Avance2ClienteServidorGrupo8 {
+import javax.swing.*;
 
+public class Avance2ClienteServidorGrupo8 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Usar la apariencia del sistema operativo para la interfaz de usuario, si es posible.
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        // Asegúrate de que la creación de la GUI se ejecute en el Event Dispatch Thread (EDT).
+        SwingUtilities.invokeLater(() -> {
+            new Bienvenida();  // Inicia con la ventana de bienvenida
+        });
     }
 }
